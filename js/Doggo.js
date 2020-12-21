c5712de3456b0e83667368ccc07dd6f8aa59b9d2
mlodyFrontendowiec <pawel.kruszelnicki@onet.pl>
2020-12-21 17:19:26 +0100

    Create buttons with informations from API

--- js/Doggo.js
+++ js/Doggo.js
@@ -3,6 +3,7 @@ export class Doggo {
     this.apiUrl = "https://dog.ceo/api";
     this.imgEl = document.querySelector(".featured-dog img");
     this.backgroundEl = document.querySelector(".featured-dog__background");
+    this.tilesEl = document.querySelector(".tiles");
   }
   listBreeds() {
     return fetch(`${this.apiUrl}/breeds/list/all`)
@@ -26,6 +27,45 @@ export class Doggo {
       this.imgEl.src = imgSrc;
       this.backgroundEl.style.background = `url(${imgSrc})`;
     });
-    this.listBreeds().then((breeds) => console.log(breeds));
+    this.showAllBreeds();
+  }
+  addBreed(breed, subBreed) {
+    let name;
+    let type;
+    if (typeof subBreed === "undefined") {
+      name = breed;
+      type = breed;
+    } else {
+      name = `${breed} ${subBreed}`;
+      type = `${breed}/${subBreed}`;
+    }
+    const tile = document.createElement("div");
+    tile.classList.add("tiles__tile");
+
+    const tileContent = document.createElement("div");
+    tileContent.classList.add("tiles__tile-content");
+
+    tileContent.innerText = name;
+    tileContent.addEventListener("click", () => {
+      this.getRandomImageByBreed(type).then((imgSrc) => {
+        this.imgEl.src = imgSrc;
+        this.backgroundEl.style.background = `url(${imgSrc})`;
+      });
+    });
+    tile.appendChild(tileContent);
+    this.tilesEl.appendChild(tile);
+  }
+  showAllBreeds() {
+    this.listBreeds().then((breeds) => {
+      for (const breed in breeds) {
+        if (breeds[breed].length === 0) {
+          this.addBreed(breed);
+        } else {
+          for (const subBreed of breeds[breed]) {
+            this.addBreed(breed, subBreed);
+          }
+        }
+      }
+    });
   }
 }
